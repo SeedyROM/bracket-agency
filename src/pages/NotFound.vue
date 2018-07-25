8039375e783790791ce0ad8a24c58b15f2f72c6c
Zack Kollar <rallokkcaz@gmail.com>
2018-07-24 20:33:51 -0700

    Added NotFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="title is-2">Page Not Found</h1>
+    <h3 class="subtitle is-3">This isn't the page you're looking for</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.title, .subtitle {
+  color: #f3f3f3;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './pages/Home.vue'
 import What from './pages/What.vue'
-
+import NotFound from './pages/NotFound.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: Home },
   { path: '/what', component: What },
+  { path: '/404', component: NotFound },
+  { path: '*', redirect: '/404' },
 ]
 
 export default new VueRouter({
